7661abef80643a4f2dbfbc0d2880ea60f1d00da2
gottedev <gottedev@gmail.com>
2019-11-11 13:06:32 +0000

    image fix

--- src/components/avatar/Avatar.js
+++ src/components/avatar/Avatar.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 const Avatar = (props) => {
   return(
     <div>
-      <img src="/github-logo.svg" />
+      <img src="./github-logo.svg" />
     </div>
   )
 }
