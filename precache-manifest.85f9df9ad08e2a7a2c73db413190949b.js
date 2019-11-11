3a393310e1beb32c4c1afdae899a90cba366c64a
gottedev <gottedev@gmail.com>
2019-11-11 12:39:33 +0000

    Updates

---
+++ precache-manifest.85f9df9ad08e2a7a2c73db413190949b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9ff179bd4eb837808211e9648bfb177",
+    "url": "/gitsearch/index.html"
+  },
+  {
+    "revision": "0ad9efb1e47531627daa",
+    "url": "/gitsearch/static/css/main.c7f4ea80.chunk.css"
+  },
+  {
+    "revision": "c16c66e52656d378118d",
+    "url": "/gitsearch/static/js/2.5687be47.chunk.js"
+  },
+  {
+    "revision": "0ad9efb1e47531627daa",
+    "url": "/gitsearch/static/js/main.7847bd98.chunk.js"
+  },
+  {
+    "revision": "6badc7e4afc797fa3220",
+    "url": "/gitsearch/static/js/runtime-main.f1f1a972.js"
+  }
+]);
