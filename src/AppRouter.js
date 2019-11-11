250d46880b454aecb616bcf2d2602cba90607e33
gottedev <gottedev@gmail.com>
2019-11-11 13:44:45 +0000

    fix:: routes

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  BrowserRouter as Router, Switch, Route
+  HashRouter as Router, Switch, Route
 } from 'react-router-dom';
 import OrganisationSearch from './gitsearch/OrgnisationSearch'
 import RepoSearch from './gitsearch/RepoSearch'
