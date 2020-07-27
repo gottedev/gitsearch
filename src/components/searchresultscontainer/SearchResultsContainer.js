ab6b4d050a561ba82a20560caa0fd1dceff10184
venkat gotte <gottedev@gmail.com>
2020-07-27 12:18:59 +0100

    fixes console errors

--- src/components/searchresultscontainer/SearchResultsContainer.js
+++ src/components/searchresultscontainer/SearchResultsContainer.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import './SearchResultsContainer.css';
-import { compose } from 'recompose';
-import { Scrollbars } from 'react-custom-scrollbars';
-import PropTypes from 'prop-types';
-import WithLoader from '../WithLoader';
+import React from "react";
+import { connect } from "react-redux";
+import "./SearchResultsContainer.css";
+import { compose } from "recompose";
+import { Scrollbars } from "react-custom-scrollbars";
+import PropTypes from "prop-types";
+import WithLoader from "../WithLoader";
 
 const SearchResultsContainer = ({ state: { data, processingStatus } }) => {
   const isDataEmpty = data.length === 0;
-  const dataProcessed = processingStatus === 'PROCESSED';
+  const dataProcessed = processingStatus === "PROCESSED";
 
-  if (processingStatus === 'processingError') {
+  if (processingStatus === "processingError") {
     return (
       <div className="ResultContainer">
         <div>Something Went Wrong!!! Try again later</div>
@@ -20,36 +20,33 @@ const SearchResultsContainer = ({ state: { data, processingStatus } }) => {
   return (
     <Scrollbars autoHide={false} autoHeight autoHeightMin="300px">
       <div className="ResultContainer">
-        { isDataEmpty && dataProcessed
-
-          ? (
-            <div>
-            No Results on Github!!! Try Something else
-            </div>
-          )
-          : (data.map((val) => (
-            <div className="Result" key={val.description}>
+        {isDataEmpty && dataProcessed ? (
+          <div>No Results on Github!!! Try Something else</div>
+        ) : (
+          data.map((val) => (
+            <div className="Result" key={val.name}>
               <div className="ResultTitle">{val.name || val.login}</div>
               <div className="ResultDescription">{val.description}</div>
             </div>
-          )))}
+          ))
+        )}
       </div>
     </Scrollbars>
   );
 };
 
-const mapStateToProps = (state) => (
-  {
-    state: state.searchReducer,
-  }
-);
+const mapStateToProps = (state) => ({
+  state: state.searchReducer,
+});
 
 SearchResultsContainer.propTypes = {
   state: PropTypes.shape({
     processingStatus: PropTypes.string.isRequired,
-    data: PropTypes.arrayOf(PropTypes.shape({
-    }).isRequired).isRequired,
+    data: PropTypes.arrayOf(PropTypes.shape({}).isRequired).isRequired,
   }).isRequired,
 };
 
-export default compose(connect(mapStateToProps, null), WithLoader)(SearchResultsContainer);
+export default compose(
+  connect(mapStateToProps, null),
+  WithLoader
+)(SearchResultsContainer);
